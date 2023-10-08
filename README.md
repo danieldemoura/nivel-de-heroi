242a7422ac2a69ad6b4bdd0cf6830fc39dc9a6f1
Daniel de Moura <nazuyamikima@gmail.com>
2023-10-08 13:11:05 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Classificador de Nível de Herói
+Esse é só um desafio proposto no **Bootcamp: Potência Tech iFood - Programação do Zero** para consolidar os conhecimentos de lógica de programação.
+
+Prévia
+<img src="./.github/preview.gif" alt="Prévia do desafio">
---
+++ index.js
@@ -0,0 +1,41 @@
+const readLine = require("readline");
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+function question(prompt) {
+    return new Promise((resolve) => {
+        rl.question(prompt, resolve);
+    })
+}
+
+async function ask() {
+    const heroiName = await question("Digite o nome do seu Herói: ");
+    const xp = await question(`\nDigite a quantidade de XP do seu Herói entre 1 mil até 10 mil de XP: `);
+    const xpOfHero = Number(xp.replace(".", ""));
+    let levelOfHero = null;
+
+
+    if(xpOfHero < 1000) levelOfHero = "Ferro";
+    else if (xpOfHero >= 1001 && xpOfHero <= 2000) levelOfHero = "Bronze";
+    else if (xpOfHero >= 2001 && xpOfHero <= 5000) levelOfHero = "Prata";
+    else if (xpOfHero >= 6001 && xpOfHero <= 7000) levelOfHero = "Ouro";
+    else if (xpOfHero >= 7001 && xpOfHero <= 8000) levelOfHero = "Platina";
+    else if (xpOfHero >= 8001 && xpOfHero <= 9000) levelOfHero = "Ascendente";
+    else if (xpOfHero >= 9001 && xpOfHero <= 10000) levelOfHero = "Imortal";
+    else if (xpOfHero >= 10001) levelOfHero = "Radiante";
+    else levelOfHero = "Valor inválido: é obrigatório digitar um valor do tipo númerico!";
+
+
+    if(xpOfHero) {
+        console.log(`\n====================\nO Herói de nome ${heroiName} está no nível ${levelOfHero}`);
+    } else {
+        console.log(`\n====================\n${levelOfHero}`);
+    }
+
+    rl.close()
+}
+
+ask();
